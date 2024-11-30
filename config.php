17f87f9abe6408d3f492550dd7f353f40ec81a64
Frozen owner <156554454+prashantsahlot@users.noreply.github.com>
2024-11-30 11:43:50 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+return [
+
+    'bot_token' => '7600588135:AAHM6E_kM-oBLsCnLt5SOJBeSmYhrFlB9rs',
+
+    'channels' => ['@frozensupport1', '@vibeshiftbots', '@frozentestcgannel'], 
+
+    'channels_on_check' => ['@frozentestcgannel','@vibeshiftbots'], 
+
+    'bonus_amount' => 2, 
+    'per_reffer_bonus' => 3, 
+
+    'min_withdraw_limit' => 10, 
+
+    'f2s_payout_mid' => 'YOUR_F2S_PAYOUT_MID', 
+
+    'f2s_payout_mkey' => 'YOUR_F2S_PAYOUT_MKEY', 
+
+    'f2s_payout_guid' => 'YOUR_F2S_PAYOUT_GUID' 
+];
